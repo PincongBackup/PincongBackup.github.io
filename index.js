639bbe6fd452aa0fc860b6d5df8bbd6d60c0cfe3
PincongBot <PincongBot@pm.me>
2019-01-12 15:32:13 +0800

    完成首页的分页功能

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
 // @ts-check
 
-let page = 0
-const n = 10
+(function () {
 
-/**
- * 显示元素
- * @param {Element} element 
- */
-const show = (element) => {
-    element.classList.remove("hidden")
-}
-
-/**
- * 隐藏元素
- * @param {Element} element 
- */
-const hide = (element) => {
-    element.classList.add("hidden")
-}
-
-const onPagination = () => {
+    let page = 0
+    const n = 10
 
     /** @type {NodeListOf<HTMLDivElement>} */
     const itemsNodeList = document.querySelectorAll(".post-body-wrap[data-pagination]")
 
     /** @type {HTMLDivElement[]} */
     const items = Array.prototype.slice.call(itemsNodeList)
-    const { length } = items
+    const itemsLength = items.length
 
-    // 显示/隐藏问题
-    items.forEach((item) => {
-        if (+item.dataset.pagination == page) {
-            show(item)
-        } else {
-            hide(item)
-        }
-    })
-
-    // 显示/隐藏上一页按钮
+    /** @type {HTMLSpanElement} */
     const prevBtn = document.querySelector(".qpool-prev")
-    if (page == 0) {
-        hide(prevBtn)
-    } else {
-        show(prevBtn)
-    }
-
-    // 显示/隐藏下一页按钮
+    /** @type {HTMLSpanElement} */
     const nextBtn = document.querySelector(".qpool-next")
-    if (page >= Math.floor(length / n)) {
-        hide(nextBtn)
-    } else {
-        show(nextBtn)
+
+    /**
+     * 显示元素
+     * @param {Element} element 
+     */
+    const show = (element) => {
+        element.classList.remove("hidden")
     }
 
-}
+    /**
+     * 隐藏元素
+     * @param {Element} element 
+     */
+    const hide = (element) => {
+        element.classList.add("hidden")
+    }
 
+    const onPagination = () => {
+
+        // 显示/隐藏问题
+        items.forEach((item) => {
+            if (+item.dataset.pagination == page) {
+                show(item)
+            } else {
+                hide(item)
+            }
+        })
+
+        // 显示/隐藏上一页按钮
+        if (page == 0) {
+            hide(prevBtn)
+        } else {
+            show(prevBtn)
+        }
+
+        // 显示/隐藏下一页按钮
+        if (page >= Math.floor(itemsLength / n)) {
+            hide(nextBtn)
+        } else {
+            show(nextBtn)
+        }
+
+    }
+
+    prevBtn.onclick = () => {
+        page = Math.max(0, page - 1)
+        onPagination()
+    }
+
+    nextBtn.onclick = () => {
+        page = Math.min(Math.floor(itemsLength / n), page + 1)
+        onPagination()
+    }
+
+    onPagination()
+
+})()
